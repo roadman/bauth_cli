9769cef74648e795cf227b89a4d9e1db0cc1e345
roadman <roadmang@gmail.com>
2017-01-26 00:09:40 +0900

    impliment: basic auth generate command

--- command/add.go
+++ command/add.go
@@ -1,7 +1,13 @@
 package command
 
 import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"os/exec"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type AddCommand struct {
@@ -9,7 +15,37 @@ type AddCommand struct {
 }
 
 func (c *AddCommand) Run(args []string) int {
-	// Write your code here
+	argsNum := len(args)
+	var generateNum int
+	if argsNum == 0 {
+		return 1
+	} else if argsNum == 2 {
+		generateNum, _ = strconv.Atoi(args[1])
+		if generateNum == 0 {
+			generateNum = 1
+		}
+	} else if argsNum > 2 {
+		return 1
+	}
+
+	if len(args[0]) == 0 {
+		return 1
+	}
+	userString := args[0]
+
+	fmt.Printf("# group: %s, created: %s\n", userString, time.Now())
+
+	var pass string
+	var user string
+	for cnt := 0; cnt < generateNum; cnt++ {
+		pass = randomString(8)
+		user = fmt.Sprintf("%s%03d", userString, cnt+1)
+		out, err := exec.Command("/usr/sbin/htpasswd", "-nbB", user, pass).Output()
+		if err != nil {
+			continue
+		}
+		fmt.Printf("%s\t# pass: %s\n", strings.TrimRight(string(out), "\n"), pass)
+	}
 
 	return 0
 }
@@ -20,7 +56,17 @@ func (c *AddCommand) Synopsis() string {
 
 func (c *AddCommand) Help() string {
 	helpText := `
-
 `
 	return strings.TrimSpace(helpText)
+}
+
+func randomString(length int) string {
+	const base = 36
+	size := big.NewInt(base)
+	n := make([]byte, length)
+	for i, _ := range n {
+		c, _ := rand.Int(rand.Reader, size)
+		n[i] = strconv.FormatInt(c.Int64(), base)[0]
+	}
+	return string(n)
 }
